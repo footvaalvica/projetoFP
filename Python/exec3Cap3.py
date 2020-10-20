175f3797eb85b090b5f62ccadf7e09ff96c11d5e
Mateus Pinho <mateusleitepinho@gmail.com>
2020-10-20 23:16:09 +0100

    we got it from here thank you

---
+++ Python/exec3Cap3.py
@@ -0,0 +1,8 @@
+raio = float(input("Raio\n"))
+
+def area_circulo(raio):
+    
+    area = 3.14 * (raio**2)
+    return area
+
+print(area_circulo(raio))
---
+++ Python/exec9.py
@@ -0,0 +1,13 @@
+num = ""
+soma = ""
+
+while True:
+    num = input("Escreva número\n")
+    
+    print(num)
+
+    if num != "-1":
+        pass
+    else:
+        print("penis")
+        break
