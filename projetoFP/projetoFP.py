04b5d5b071032b1d0d9fe88a0f163330c7638f7d
Mateus Pinho <mateusleitepinho@gmail.com>
2020-11-06 18:43:16 +0000

    cockers

---
+++ projetoFP/projetoFP.py
@@ -0,0 +1,37 @@
+#Mateus Leite Pinho ist199282
+
+def eh_tabuleiro(tab):
+    Truth = False
+    if isinstance(tab, tuple) and len(tab) == 3:
+        for i in tab:
+            if len(i) == 3:
+                for i2 in i:
+                    if i2 == 1 or i2 == 0 or i2 == -1:
+                        Truth = True
+                    else:
+                        return False
+            else:
+                return False
+    else:
+        return False
+    return Truth
+
+def eh_posicao(pos):
+    return isinstance(pos, int) and 1 <= pos <= 9
+
+def obter_coluna(tab, num):
+    coluna = ()
+    if (eh_tabuleiro(tab) and isinstance(num, int) and 1 <= num <= 3) == True:
+        num -= 1
+        for i in tab:
+            coluna += (i[num],)
+        return coluna
+    else:
+        raise ValueError("caca")
+
+def obter_linha(tab, num):
+    if (eh_tabuleiro(tab) and isinstance(num, int) and 1 <= num <= 3) == True:
+        num -= 1
+        return tab[num]
+    else:
+        raise ValueError("caca")
