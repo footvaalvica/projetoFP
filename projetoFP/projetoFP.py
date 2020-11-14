ba2280ff783e8f331b88625d6b9f478b784ab096
Mateus Pinho <mateusleitepinho@gmail.com>
2020-11-14 12:25:19 +0000

    now YALL GET THE MESSAGE

--- projetoFP/projetoFP.py
+++ projetoFP/projetoFP.py
@@ -57,45 +57,42 @@ def obter_diagonal(tab, num):
         raise ValueError("caca")
 
 def tabuleiro_str(tab):
-
-    """A função está super macabra, basicamente estou a transformar a string numa lista
+    """
+    A função está super macabra, basicamente estou a transformar a string numa lista
     e a inserir nas posições certas
     """
-
-    tabGood2 = []
+    tabComX = []
     tabPos = (1,5,9,25,29,33,49,53,57)
     counter = 0
     corda = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
-    cordaList = []
     newCorda = ""
     if eh_tabuleiro(tab) == False:
         raise ValueError("caca")
-    else:
-        cordaList = list(corda)
-        tabGood = tabuleiroMelhor(tab)
-        for i in tabGood:
-            if i == 1:
-                i = "X"
-            elif i == 0:
-                i = " "
-            elif i == -1:
-                i = "O"
-            tabGood2.append(i)
-        while counter < len(tabGood2):
-            cordaList[tabPos[counter]] = tabGood2[counter]
-            counter += 1
-        for i in cordaList:
-            newCorda += str(i)
-        print(newCorda)
+    corda = list(corda)
+    tabGood = tabuleiroMelhor(tab)
+    for i in tabGood:
+        if i == 1:
+            i = "X"
+        elif i == 0:
+            i = " "
+        elif i == -1:
+            i = "O"
+        tabComX.append(i)
+    while counter < len(tabComX):
+        corda[tabPos[counter]] = tabComX[counter]
+        counter += 1
+    for i in corda:
+        newCorda += str(i)
+    return newCorda
 
 def eh_posicao_livre(tab,pos):
     tabGood = tabuleiroMelhor(tab)
-    if eh_tabuleiro(tab) == False and eh_posicao(pos) == False:
+    if eh_tabuleiro(tab) == False or eh_posicao(pos) == False:
         raise ValueError("eh_posicao_livre:  algum dos argumentos e invalido")
     else:
         counter = 0
         while counter < len(tabGood):
-            if counter == pos and tabGood[counter] == 0:
+            if (counter + 1) == pos and tabGood[counter] == 0:
                 return True
             counter += 1
         return False
@@ -106,7 +103,7 @@ def obter_posicoes_livres(tab):
         raise ValueError("obter_posicoes_livres:  o argumento e invalido")
     for i in range(9):
         if eh_posicao_livre(tab,(i+1)) == True:
-            tuploNovo += ((i+1),)
+            tuploPosicoesLivres += ((i+1),)
     return tuploPosicoesLivres
 
 def jogador_ganhador(tab):
@@ -136,4 +133,23 @@ def jogador_ganhador(tab):
                 return 0
 
 def marcar_posicao(tab,n,pos):
+    if eh_posicao_livre(tab,pos) == False or n != 1 or n != 0 or n != -1:
+        raise ValueError("marcar_posicao: algum dos argumentos e invalido")
+    else:
+        tabGood = tabuleiroMelhor(tab)
+        tabGood = list(tabGood)
+        tabGood[pos-1] = n
+    return tuple(tabGood[:3]),tuple(tabGood[3:6]),tuple(tabGood[6:9])
+
+def escolher_posicao_manual(tab):
+    if eh_tabuleiro(tab) == False:
+        raise ValueError('escolher_posicao_manual: o argumento e invalido')
+    x = int(input("Turno do jogador.  Escolha uma posicao livre: "))
+    if eh_posicao_livre(tab,x) == False:
+        raise ValueError('escolher_posicao_manual: a posicao introduzida e invalida')
+    return x
+
+def escolher_posicao_auto(tab,n,str):
+    if eh_tabuleiro(tab) == False or n != 1 or n != 0 or n != 1 or str != "basico" or str != "normal" or str != "perfeito":
+        raise ValueError("escolher_posicao_auto: algum dos argumentos e invalido")
     pass
